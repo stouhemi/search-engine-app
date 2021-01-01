e24c6e57ee566f2a836e430f4335c4fbebc2159d
mdnfiras <mdnfiras@yahoo.com>
2021-01-02 00:09:16 +0100

    still trying other stuff

--- search.cpp
+++ search.cpp
@@ -12,7 +12,7 @@ int main(){
     }
 
     // Experimental
-    //sort(t, t+N);
+    sort(t, t+N);
 
     freopen("IN.txt", "r", stdin);
     freopen("RES.txt", "w", stdout);
@@ -27,8 +27,8 @@ int main(){
         // Current method
         
         // Experimental
-        //f = binary_search(t, t+N, x);
+        f = binary_search(t, t+N, x);
         
-        couti << true << '\n';
+        cout << true << '\n';
     }
 }
